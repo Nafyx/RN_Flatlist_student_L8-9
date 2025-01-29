f66555cf9d6835c583357c6cb5959f0df8fe99a0
Nafyx <122580098+Nafyx@users.noreply.github.com>
2025-01-29 08:18:45 -0600

    worked on list

--- flatlistApp/app/index.tsx
+++ flatlistApp/app/index.tsx
@@ -7,16 +7,56 @@ import {
 } from "react-native";
 import colors from "../styles/colors";
 import defaultStyles from "../styles/defaultStyles";
+import {useState} from "react"
 
 export default function Index() {
+  const [selectedId, setSelectedId] = useState<string>("")
+
+  type dataType = {
+    id: string; // unique identifier
+    title: string; // text we see in flatlist
+  }
+
+  const DATA: dataType[] = [
+    {id: '1', title: 'First'},
+    {id: '2', title: 'Second'},
+    {id: '3', title: "Third"},
+    {id: '4', title: "Fourth"},
+    {id: '5', title: "Fifth"},
+  ];
+
+  /*
+  declaring a function called selectedList that receives
+  a parameter of type dataType that we will refer to as 'item'
+  I can access the values of item using dot notation
+  */
+  const selectedList = (item: dataType) => {
+    setSelectedId(item.id);
+    console.log("selected " + item.title);
+  }
+
   return (
     <View style={defaultStyles.container}>
       <View style={defaultStyles.titleContainer}>
-        <Text style={defaultStyles.title}>Insert Title Here</Text>
+        <Text style={defaultStyles.title}>1st Per Flatlist</Text>
       </View>
       <View style={[defaultStyles.textContainer, { flex: 1 }]}>
         <View style={styles.flatlist}>
-          <Text>This is where our list will go</Text>
+          <FlatList 
+            data={DATA}
+            keyExtractor={(item: dataType) => item.id}
+            renderItem={({item}) => (
+              <TouchableOpacity onPress={() => selectedList(item)}>
+                <View style={[styles.titleContainer, {
+                  backgroundColor: 
+                    item.id === selectedId ? colors.primary : colors.secondary
+                }
+                ]}>
+                  <Text style={styles.titleText}>{item.title}</Text>
+                </View>
+              </TouchableOpacity>
+            )}
+          />
         </View>
       </View>
     </View>
@@ -32,6 +72,7 @@ const styles = StyleSheet.create({
     width: 300,
     borderTopLeftRadius: 15,
     borderTopRightRadius: 15,
+    backgroundColor: "lightblue"
   },
   titleText: {
     fontSize: 24,
